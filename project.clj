47e95bc23f6091bfc5045dd86df166d162e2d3fa
jao <jao@bigml.com>
2019-07-05 18:41:16 +0100

    project.clj updated for bigml-specific release

--- project.clj
+++ project.clj
@@ -1,22 +1,8 @@
-(defproject lein-modules "0.3.12-SNAPSHOT"
-  :description "Similar to Maven multi-module projects, but less sucky"
-  :url "https://github.com/jcrossley3/lein-modules"
+(defproject bigml/lein-modules "0.3.12"
+  :description "Little extension to lein-modules to propagate project versions"
+  :url "https://github.com/bigmlcom/lein-modules"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
   :aliases {"all" ["do" "clean," "test," "install"]}
-  :signing {:gpg-key "92439EF5"}
-  :plugins [[lein-file-replace "0.1.0"]]
-  :deploy-repositories {"releases" :clojars}
-  :release-tasks
-  [["vcs" "assert-committed"]
-   ["change" "version" "leiningen.release/bump-version" "release"]
-
-   ["file-replace" "README.md" "lein-modules \"" "\"]" "version"]
-
-   ["vcs" "commit"]
-   ["vcs" "tag"]
-   ["deploy"]
-   ["change" "version" "leiningen.release/bump-version"]
-   ["vcs" "commit"]
-   ["vcs" "push"]])
+  :deploy-repositories {"releases" :clojars})
